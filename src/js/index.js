d687a10252c30f21528f3614a305e69d53f12760
Battur <battur.ts@empasoft.mn>
2021-02-21 01:53:10 +0800

    search bolon state controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,33 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+/*
+web апп төлөв 
+- хайлтын query, үр дүн
+- тухайн үзүүлж байгаа жор
+- лайкдсан жорууд
+- захиалж байгаа жорын найрлагууд
 
-search.doSearch().then((r) => console.log(r));
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1. webees hailtiin tulhuur ug gargaj avna
+  const query = "pasta";
+
+  if (query) {
+    // 2. shineer hailtiin obectiig uusgej ugnu
+    state.search = new Search(query);
+    // 3. hailt hiihed zoriulj delgetsiin UI iin beldene
+    // 4. hailtiig guitsetgene
+    await state.search.doSearch();
+    // 5. ur dung delgetsen uzuulne
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
