e9a15d7350347a5acbd466f38f853876ed50b410
Battur <battur.ts@empasoft.mn>
2021-02-22 15:00:01 +0800

    Completed Project

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  //mode: "development",
   entry: "./src/js/index.js",
   output: {
     filename: "js/main.js",
@@ -11,7 +11,7 @@ module.exports = {
   devServer: {
     contentBase: "./docs",
   },
-  devtool: "inline-source-map",
+  //devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
